d0fa690396ac028307635d5b08561d74bc8253b1
wstudionatali <wstudionatali@gmail.com>
2024-08-24 21:53:04 +0200

    create routes for home and actions: scan, remove

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('scan/{product_code}', [\App\Http\Controllers\HomeController::class, 'scan'])->name('scan');
+Route::get('remove/{product_code}', [\App\Http\Controllers\HomeController::class, 'remove'])->name('remove');
+Route::get('remove_all/', [\App\Http\Controllers\HomeController::class, 'remove_all'])->name('remove_all');
