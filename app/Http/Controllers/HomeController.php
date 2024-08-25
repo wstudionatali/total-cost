c761cc757e0212e23e12e27ead80c604594eec7b
wstudionatali <wstudionatali@gmail.com>
2024-08-25 16:59:01 +0200

    change ordinary CheckoutService class to real-time Facade and call all methods staticaly

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,20 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Services\CheckoutService;
+use Facades\App\Services\CheckoutService;
 class HomeController extends Controller
 {
     public function index ()
-    {   //session()->forget('cart');
+    {
         $products = Product::all();
-        $total_price = (new CheckoutService)->getTotal();
-        $cart_product_codes = (new CheckoutService)->getProductsQuantity();
+        $total_price = CheckoutService::getTotal();
+        $cart_product_codes = CheckoutService::getProductsQuantity();
       return view('home', compact('products', 'total_price', 'cart_product_codes'));
 
     }
     public function scan ($product_code)
     {
-      (new CheckoutService)->scan($product_code);
+      CheckoutService::scan($product_code);
       return redirect(route('home'));
     }
     public function remove_all ()
@@ -28,7 +28,7 @@ class HomeController extends Controller
     }
     public function remove ($product_code)
     {
-      (new CheckoutService)->remove($product_code);
+      CheckoutService::remove($product_code);
       return redirect(route('home'));
     }
 }
